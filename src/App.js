dbc88609b95f4c58f346d64f1a84490c0a7ac38c
Rashmi <rashmi.sinha1st@gmail.com>
2023-03-21 18:59:27 +0530

    addedFooter and contcatus page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import Navbar from './Navbar/Navbar';
 import './App.css';
 //import Section from './Section1/Section'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Blogs from './Blogs/Blogs';
+import Footer from './Footer/Footer'
+import Contact from './Contact/Contact';
 
 function App() {
   return (
     <div className="App">
-     <Navbar></Navbar>
-     <Blogs/>
+    
+      <Router>
+      <Navbar></Navbar>
+        <Routes>
+          <Route path='/' index element={<Blogs/>}></Route>
+          <Route path='/contactUs'exact element={<Contact/>}></Route>
+        </Routes>
+      </Router>
+      <Footer></Footer>
     </div>
   );
 }
